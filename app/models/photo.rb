fc4b62ed7f5d193a787794d3681332fc05b8a501
akio0911 <akio0911@gmail.com>
2009-07-20 02:49:35 +0900

    Photoのクロール処理を追加

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,15 +1,30 @@
 require 'open-uri'
 require 'rss'
+require "rexml/document"
 class Photo < ActiveRecord::Base
   belongs_to :user
+
   def self.import_flickr
-    url = "http://api.flickr.com/services/feeds/photos_public.gne?id=84889587@N00&lang=en-us&format=rss_200"
-    open(url) do | http |
-      response = http.read
-      rss_items = RSS::Parser.parse(response, false)
-      rss_items.items.each do  |rss_item|
-        unless Photo.find(:first, :conditions => ["url = ?", rss_item.link])
-          Photo.create(:value => rss_item.title , :description => rss_item.description, :link => rss_item.link, :pub_date => rss_item.pubDate.strftime("%Y%m%d %H%M%S"))
+    User.find(:all).each do |u|
+      url = u.flickr_feed_url
+
+#       open(url) do | http |
+#         response = http.read
+#         rss_items = RSS::Parser.parse(response, false)
+#         rss_items.items.each do  |rss_item|
+#           unless Photo.find(:first, :conditions => ["url = ?", rss_item.link])
+#             Photo.create(:url => rss_item.link, :user_id => u.id)
+#           end
+#         end
+#       end
+
+      xml = open("http://api.flickr.com/services/feeds/photos_public.gne?id=17653213@N05&amp;lang=en-us&amp;format=rss_200").read
+      doc = REXML::Document.new xml
+      doc.elements.each('/feed') do |feed|
+        feed.elements.each('entry') do |entry|
+          entry.elements.each('link') do |link|
+            p link.attributes['href']
+          end
         end
       end
     end
