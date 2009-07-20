ed719ef8a007eec683c62e861788dd04aaa4c799
akio0911 <akio0911@gmail.com>
2009-07-20 11:54:43 +0900

    Photoのクロール処理を追加

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,23 +7,17 @@ class Photo < ActiveRecord::Base
   def self.import_flickr
     User.find(:all).each do |u|
       url = u.flickr_feed_url
-
-#       open(url) do | http |
-#         response = http.read
-#         rss_items = RSS::Parser.parse(response, false)
-#         rss_items.items.each do  |rss_item|
-#           unless Photo.find(:first, :conditions => ["url = ?", rss_item.link])
-#             Photo.create(:url => rss_item.link, :user_id => u.id)
-#           end
-#         end
-#       end
-
-      xml = open("http://api.flickr.com/services/feeds/photos_public.gne?id=17653213@N05&amp;lang=en-us&amp;format=rss_200").read
+      xml = open(url).read
       doc = REXML::Document.new xml
       doc.elements.each('/feed') do |feed|
         feed.elements.each('entry') do |entry|
           entry.elements.each('link') do |link|
-            p link.attributes['href']
+            if link.attributes['rel'] == "enclosure"
+              photo = Photo.new
+              photo.url = link.attributes['href']
+              photo.user_id = u.id
+              photo.save
+            end
           end
         end
       end
