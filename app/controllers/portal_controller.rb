3143d73dd5d126321a5d10f7347b34631e605684
akio0911 <akio0911@gmail.com>
2009-07-20 12:03:09 +0900

    portal/index.html.erbを追加

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -1,10 +1,11 @@
 class PortalController < ApplicationController
   def index
-    @groups = Group.find(:all, :order => "start")
-    respond_to do |format| 
-      format.html { render :action => "index"}
-      format.xml  { render :xml    => @groups}
-    end
+#     @groups = Group.find(:all, :order => "start")
+#     respond_to do |format| 
+#       format.html { render :action => "index"}
+#       format.xml  { render :xml    => @groups}
+#     end
+    @groups = Group.find(:all)
   end
 end
 
